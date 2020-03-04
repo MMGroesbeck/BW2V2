a45cf6ce34a70f0a8fe330b4070c43917e920d77
M Groesbeck <m.groesbeck@gmail.com>
2020-03-04 10:54:51 -0800

    Initial commit (skeleton).

---
+++ anywherefit/src/dummyData.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const startData = [
+    {
+        name: "Core Training for Beginners",
+        type: "core",
+        start: "10:00 am",
+        duration: "45 min",
+        intensity: "low",
+        location: "Trolley Barn Park",
+        registered: 7,
+        max: 15,
+        instructor: "Tak Loufer",
+        id: 1
+    }
+]
+
+export default startData;
